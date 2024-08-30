3dbd662e3a04f0eb9d49a296a70a600ee15190a7
Ahmad2243 <ahamdrabbani2243@gmail.com>
2024-08-30 16:44:43 +0500

    applying tailwind css on personal detail component

--- cv-maker/src/Personal.tsx
+++ cv-maker/src/Personal.tsx
@@ -14,51 +14,57 @@ const Personal = ({
   return (
     <>
       <div id="personal-backdrop" onClick={onClose}></div>
-      <div className="personal-box">
+      <div className="p-2">
         <form>
-          <h2>Full Name:</h2>
+          <h2 className="font-bold">Full Name:</h2>
           <input
-            className="inputName"
+            className="border-gray-100 border-2 px-2 mb-4 mx-2"
             type="text"
             value={inputName}
             onChange={onInputNameChange}
             placeholder="Enter your name"
           />
-          <h2>Job Title:</h2>
+          <h2 className="font-bold">Job Title:</h2>
           <input
-            className="inputJobTitle"
+            className="border-gray-100 border-2 px-2 mb-4 mx-2"
             type="text"
             value={inputTitle}
             onChange={onInputTitleChange}
             placeholder="Enter your job title"
           />
-          <h2>Phone Number:</h2>
+          <h2 className="font-bold">Phone Number:</h2>
           <input
-            className="inputNumber"
+            className="border-gray-100 border-2 px-2 mb-4 mx-2"
             type="text"
             value={inputNumber}
             onChange={onInputNumberChange}
             placeholder="Enter your phone Number"
           />
-          <h2>E-mail:</h2>
+          <h2 className="font-bold">E-mail:</h2>
           <input
-            className="inputEmail"
+            className="border-gray-100 border-2 px-2 mb-4 mx-2"
             type="text"
             value={inputEmail}
             onChange={onInputEmailChange}
             placeholder="Enter your Phone Number"
           />
-          <h2>E-mail:</h2>
+          <h2 className="font-bold">E-mail:</h2>
           <input
-            className="inputLocation"
+            className="border-gray-100 border-2 px-2 mb-4 mx-2"
             type="text"
             value={inputLocation}
             onChange={onInputLocationChange}
             placeholder="Enter your Location"
           />
-          <button id="btn1" type="button" onClick={onClose}>
-            Close
-          </button>
+          <div className="flex w-[310px] justify-center">
+            <button
+              className="border-2 border-black rounded-[50px] cursor-pointer hover:bg-slate-100 hover:border-gray-400"
+              type="button"
+              onClick={onClose}
+            >
+              Close
+            </button>
+          </div>
         </form>
       </div>
     </>
