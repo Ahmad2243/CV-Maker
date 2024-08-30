dae832ec208d5947c93f2d7a89f8cb6a8a26f027
Ahmad2243 <ahamdrabbani2243@gmail.com>
2024-08-30 18:01:31 +0500

    apply tailwind css on professional detail component

--- cv-maker/src/Professional.tsx
+++ cv-maker/src/Professional.tsx
@@ -18,56 +18,71 @@ const Professional = ({
       <div id="Professional-backdrop" onClick={onClose}></div>
       <div id="Professional-box">
         <form className="formProfessional" onSubmit={onProSubmit}>
-          <h2>Job Title:</h2>
+          <h2 className="font-bold">Job Title:</h2>
           <input
-            className="inputJobTitle"
+            className="border-gray-100 border-2 px-2 mb-4 mx-2"
             type="text"
             value={inputJobTitle}
             onChange={onInputJobTitleChange}
             placeholder="Enter your Job Title"
           />
-          <h2>Company Name:</h2>
+          <h2 className="font-bold">Company Name:</h2>
           <input
-            className="inputCompanyName"
+            className="border-gray-100 border-2 px-2 mb-4 mx-2"
             type="text"
             value={inputCompanyName}
             onChange={onInputCompanyNameChange}
             placeholder="Enter your Company Name"
           />
-          <h2>Start Date:</h2>
+          <h2 className="font-bold">Start Date:</h2>
           <input
-            className="inputStartDate"
+            className="border-gray-100 border-2 px-2 mb-4 mx-2"
             type="text"
             value={inputProStartDate}
             onChange={onInputProStartDateChange}
             placeholder="Enter your Start Date"
           />
-          <h2>End Date:</h2>
+          <h2 className="font-bold">End Date:</h2>
           <input
-            className="inputEndDate"
+            className="border-gray-100 border-2 px-2 mb-4 mx-2"
             type="text"
             value={inputProEndDate}
             onChange={onInputProEndDateChange}
             placeholder="Enter your End Date"
           />
-          <h2>Description:</h2>
+          <h2 className="font-bold">Description:</h2>
           <input
-            className="inputDescription"
+            className="border-gray-100 border-2 px-2 mb-4 mx-2"
             type="text"
             value={inputDescription}
             onChange={onInputDescriptionChange}
             placeholder="Enter your Description"
           />
-          <div id="formProfessionalButtons">
-            <button id="btn1" type="button" onClick={onClose}>
-              Close
-            </button>
-            <button type="button" id="dustbin" onClick={onProDustbinClick}>
-              🗑️
-            </button>
-            <button id="btn2" type="submit">
-              Submit
-            </button>
+          <div className="flex w-[290px] justify-between">
+            <div>
+              <button
+                className="border-2 border-black rounded-[50px] cursor-pointer hover:bg-slate-100 hover:border-gray-400"
+                type="button"
+                onClick={onClose}
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                className="bg-white hover:border-transparent px-0 py-0 m-2"
+                onClick={onProDustbinClick}
+              >
+                🗑️
+              </button>
+            </div>
+            <div>
+              <button
+                className="bg-blue-500 border-2 border-black rounded-[50px] cursor-pointer hover:bg-slate-100 hover:border-gray-400"
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
           </div>
         </form>
       </div>
