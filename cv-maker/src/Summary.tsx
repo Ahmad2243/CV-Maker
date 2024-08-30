754a1b94731037e92fc89ca3700be6cdfd7486c2
Ahmad2243 <ahamdrabbani2243@gmail.com>
2024-08-30 17:05:57 +0500

    apply tailwind css in summary component

--- cv-maker/src/Summary.tsx
+++ cv-maker/src/Summary.tsx
@@ -5,15 +5,21 @@ const Summary = ({ inputValue, onInputChange, onClose }) => {
       <div className="summary-box">
         <form className="formSummary">
           <input
-            className="inputValue"
+            className="border-gray-100 border-2 px-2 mr-2 mb-4 mx-2 w-[290px] h-20"
             type="text"
             value={inputValue}
             onChange={onInputChange}
             placeholder="Enter your summary"
           />
-          <button id="btn1" type="button" onClick={onClose}>
-            Close
-          </button>
+          <div className="flex w-[310px] justify-center">
+            <button
+              className="border-2 border-black rounded-[50px] cursor-pointer hover:bg-slate-100 hover:border-gray-400"
+              type="button"
+              onClick={onClose}
+            >
+              Close
+            </button>
+          </div>
         </form>
       </div>
     </>
